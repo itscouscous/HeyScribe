a22c571be9b4fc2ec7e432d7edba8246e8a37851
Shayde <shayderade@gmail.com>
2022-04-26 13:34:45 -0500

    minor edits to app.js, made html more uniform

---
+++ app.js
@@ -0,0 +1,30 @@
+//Declaring ALL of our elements
+//Navbar
+let linkwork = document.querySelector("#link-work");
+let linkaboutus = document.querySelector("#link-abous-us");
+let useremail = document.querySelector("#user_email");
+let btnlogin = document.querySelector("#btn-log-in");
+let btnsignup = document.querySelector("#btn-sign-up");
+//Search Page
+let searchbar = document.querySelector("#search_bar"); //Search page will need more elements, still has some restaurant template IDs
+let searchbutton = document.querySelector("#search_button");
+let filterform = document.querySelector("#filter_form");
+//Signup
+let signup = document.querySelector("#sign-up");
+let email = document.querySelector("#email");
+let password = document.querySelector("#password");
+let btnsignupsubmit = document.querySelector("#btn-sign-up-submit");
+let linkogin = document.querySelector("#link-log-in");
+//About Us probably needs elements for the Contact Us form still
+//Account Type
+let accounttypeform = document.querySelector("#accounttype_form");
+//My Account (Client)
+let modalone = document.querySelector("#modalOne");
+let legal = document.querySelector("#legal");
+let entertainment = document.querySelector("#entertainment");
+let education = document.querySelector("#education");
+let medical = document.querySelector("#medical");
+let criteria = document.querySelector("#criteria");
+let deadline = document.querySelector("#deadline");
+let docpicker = document.querySelector("#docpicker");
+//My Account (Scribe)
