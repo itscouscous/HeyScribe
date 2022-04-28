bbafc8d2e9f170543c87d2bd34b358e994bd2108
Shayde <shayderade@gmail.com>
2022-04-28 13:02:08 -0500

    slight edits to comments

--- public/app.js
+++ public/app.js
@@ -1,108 +1,105 @@
 //Declaring ALL of our elements
-    //Page Declarations
-    let mainpage = document.querySelector("#main-page"); //this is our "home page"
-    let signup = document.querySelector("#sign-up");
-    let aboutus = document.querySelector("#about-us");
-    let myaccountclient = document.querySelector("#client-my-account");
-    let myaccountscribe = document.querySelector("#scribe-my-account");
-    let searchpage = document.querySelector("#discover");
-    //Navbar
-    let logo = document.querySelector("#logo");
-    let linkwork = document.querySelector("#link-work");
-    let linkaboutus = document.querySelector("#link-about-us");
-    let useremail = document.querySelector("#user_email");
-    let btnlogin = document.querySelector("#btn-log-in");
-    let btnsignup = document.querySelector("#btn-sign-up");
-    //Search Page
-    let searchbar = document.querySelector("#search_bar"); //Search page will need more elements, still has some restaurant template IDs
-    let searchbutton = document.querySelector("#search_button");
-    let filterform = document.querySelector("#filter_form");
-    //Signup
-    let email = document.querySelector("#email");
-    let password = document.querySelector("#password");
-    let confirmpassword = document.querySelector("#confirmpassword");
-    let btnsignupsubmit = document.querySelector("#btn-sign-up-submit");
-    let linkogin = document.querySelector("#link-log-in");
-    let signupform = document.querySelector("#signup_form");
-    //About Us probably needs elements for the Contact Us form still
-    //Account Type
-    let accounttypeform = document.querySelector("#accounttype_form");
-    //My Account (Client)
-    let modalone = document.querySelector("#modalOne");
-    let legal = document.querySelector("#legal");
-    let entertainment = document.querySelector("#entertainment");
-    let education = document.querySelector("#education");
-    let medical = document.querySelector("#medical");
-    let criteria = document.querySelector("#criteria");
-    let deadline = document.querySelector("#deadline");
-    let docpicker = document.querySelector("#docpicker");
-    //My Account (Scribe)
+//Page Declarations
+let mainpage = document.querySelector("#main-page"); //this is our "home page"
+let signup = document.querySelector("#sign-up");
+let aboutus = document.querySelector("#about-us");
+let myaccountclient = document.querySelector("#client-my-account");
+let myaccountscribe = document.querySelector("#scribe-my-account");
+let searchpage = document.querySelector("#discover");
+//Navbar
+let logo = document.querySelector("#logo");
+let linkwork = document.querySelector("#link-work");
+let linkaboutus = document.querySelector("#link-about-us");
+let useremail = document.querySelector("#user_email");
+let btnlogin = document.querySelector("#btn-log-in");
+let btnsignup = document.querySelector("#btn-sign-up");
+//Search Page
+let searchbar = document.querySelector("#search_bar"); //Search page will need more elements, still has some restaurant template IDs
+let searchbutton = document.querySelector("#search_button");
+let filterform = document.querySelector("#filter_form");
+//Signup
+let email = document.querySelector("#email");
+let password = document.querySelector("#password");
+let confirmpassword = document.querySelector("#confirmpassword");
+let btnsignupsubmit = document.querySelector("#btn-sign-up-submit");
+let linkogin = document.querySelector("#link-log-in");
+let signupform = document.querySelector("#signup_form");
+//About Us probably needs elements for the Contact Us form still
+//Account Type
+let accounttypeform = document.querySelector("#accounttype_form");
+//My Account (Client)
+let modalone = document.querySelector("#modalOne");
+let legal = document.querySelector("#legal");
+let entertainment = document.querySelector("#entertainment");
+let education = document.querySelector("#education");
+let medical = document.querySelector("#medical");
+let criteria = document.querySelector("#criteria");
+let deadline = document.querySelector("#deadline");
+let docpicker = document.querySelector("#docpicker");
+//My Account (Scribe)
 
 //Navigation Events
-    //Function for navigating pages (I'm so proud of this even though the logic is super simple)
-    all_pages = [mainpage, signup, aboutus, myaccountclient, myaccountscribe, searchpage]
+//Function for navigating pages
+all_pages = [mainpage, signup, aboutus, myaccountclient, myaccountscribe, searchpage]
 
-    function navigate(button, destination) {
-        button.addEventListener('click', () => {
-            all_pages.forEach(i => {
-                if (i.classList.contains("is-hidden")) {
-                    return
-                }
-                else {
-                    i.classList.add('is-hidden');
-                }
-            });
-            destination.classList.remove("is-hidden");
+function navigate(button, destination) {
+    button.addEventListener('click', () => {
+        all_pages.forEach(i => {
+            if (i.classList.contains("is-hidden")) {
+                return
+            } else {
+                i.classList.add('is-hidden');
+            }
         });
-    }
+        destination.classList.remove("is-hidden");
+    });
+}
 
-    //Click Logo
-    navigate(logo, mainpage)
+//Click Logo
+navigate(logo, mainpage)
 
-    //Click "Find Work"
-    navigate(linkwork, searchpage)
+//Click "Find Work"
+navigate(linkwork, searchpage)
 
-    //Click "About Us"
-    navigate(linkaboutus, aboutus)
+//Click "About Us"
+navigate(linkaboutus, aboutus)
 
-    //Click "Log In" 
-        // ---> We still need login page
-    //navigate(btnlogin, login)
+//Click "Log In" 
+// ---> We still need login page
+//navigate(btnlogin, login)
 
-    //Click "Sign Up"
-    navigate(btnsignup, signup)
+//Click "Sign Up"
+navigate(btnsignup, signup)
 
 //Signup Functionality
-    signupform.addEventListener('submit', (e) => {
+signupform.addEventListener('submit', (e) => {
     //prevent auto refresh on the page
     e.preventDefault();
-  
+
     // grab email and password
     const email = document.querySelector('#email').value;
     const password = document.querySelector('#password').value;
     const confirmpassword = document.querySelector("#confirmpassword").value
-  
-    if(password == confirmpassword) {
-    auth.createUserWithEmailAndPassword(email, password).then(credentials => {
-      console.log(`UID: ${credentials.user.uid} Email: ${credentials.user.email} has signed up`);
-      signup.classList.add("is-hidden");
-      mainpage.classList.remove('is-hidden');
-  
-      // reset the form
-      signup_form.reset();
-    }).catch(err => {
-  
-      // display error message on modal
-  
-      const error = document.querySelector('.error');
-      error.innerHTML = `<p>${err.message}</p>`;
-    })}
-  
-    else{const error2 = document.querySelector('.error2');
-      error2.innerHTML = `<p>Please retry password confirmation.</p>`;
-  
-    }
-  
-  })
 
-        
+    if (password == confirmpassword) {
+        auth.createUserWithEmailAndPassword(email, password).then(credentials => {
+            console.log(`UID: ${credentials.user.uid} Email: ${credentials.user.email} has signed up`);
+            signup.classList.add("is-hidden");
+            mainpage.classList.remove('is-hidden');
+
+            // reset the form
+            signup_form.reset();
+        }).catch(err => {
+
+            // display error message on modal
+
+            const error = document.querySelector('.error');
+            error.innerHTML = `<p>${err.message}</p>`;
+        })
+    } else {
+        const error2 = document.querySelector('.error2');
+        error2.innerHTML = `<p>Please retry password confirmation.</p>`;
+
+    }
+
+})
