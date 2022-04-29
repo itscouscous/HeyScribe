5b2e56a0a2f85579a1008a558c4a467596ad14d6
mowzerkitty <olivia.aumann@gmail.com>
2022-04-29 13:56:36 -0500

    added login modal, can now login

--- public/app.js
+++ public/app.js
@@ -14,6 +14,7 @@ let linkaboutus = document.querySelector("#link-about-us");
 let useremail = document.querySelector("#user_email");
 let btnlogin = document.querySelector("#btn-log-in");
 let btnsignup = document.querySelector("#btn-sign-up");
+let linksignup = document.querySelector("#link-sign-up");
 //Search Page
 let searchbar = document.querySelector("#search_bar"); //Search page will need more elements, still has some restaurant template IDs
 let searchbutton = document.querySelector("#search_button");
@@ -26,6 +27,14 @@ let confirmpassword = document.querySelector("#confirmpassword");
 let btnsignupsubmit = document.querySelector("#btn-sign-up-submit");
 let linkogin = document.querySelector("#link-log-in");
 let signupform = document.querySelector("#signup_form");
+//login
+let loginmodal = document.querySelector("#login-modal")
+let loginmodalbg = document.querySelector("#login-modalbg")
+let loginform = document.querySelector("#login_form")
+let log_email = document.querySelector("#log_email")
+let log_password = document.querySelector("#log_password")
+let log_submitbtn = document.querySelector("#log_submitbtn")
+let login_error = document.querySelector("#login_error")
 //About Us probably needs elements for the Contact Us form still
 //Account Type
 let accounttypeform = document.querySelector("#accounttype_form");
@@ -47,6 +56,10 @@ all_pages = [mainpage, signup, aboutus, myaccountclient, myaccountscribe, search
 
 function navigate(button, destination) {
     button.addEventListener('click', () => {
+        if (destination = signup) {
+            //close login modal if open
+            close_modal("login-modal")
+        }
         all_pages.forEach(i => {
             if (i.classList.contains("is-hidden")) {
                 return
@@ -72,10 +85,11 @@ navigate(linkaboutus, aboutus)
 
 //Click "Log In" 
 // ---> We still need login page
-navigate(btnlogin, login)
+//navigate(btnlogin, login)
 
 //Click "Sign Up"
 navigate(btnsignup, signup)
+navigate(linksignup, signup)
 
 //Signup Functionality
 signupform.addEventListener('submit', (e) => {
@@ -89,12 +103,11 @@ signupform.addEventListener('submit', (e) => {
 
     if (password == confirmpassword) {
         auth.createUserWithEmailAndPassword(email, password).then(credentials => {
-            console.log(`UID: ${credentials.user.uid} Email: ${credentials.user.email} has signed up`);
             signup.classList.add("is-hidden");
             mainpage.classList.remove('is-hidden');
 
             // reset the form
-            signup_form.reset();
+            login_form.reset();
         }).catch(err => {
 
             // display error message on modal
@@ -116,10 +129,45 @@ signupform.addEventListener('submit', (e) => {
 //    - show either find work or post a job depending on user type
 //    - different homepage?
 auth.onAuthStateChanged((user) => {
-    //if user is not null (user is signed in, there is a user)
-    if (user) {
-        alert(`you are logged in as ${user.email}`)
-    } else {
-        alert('you are logged out')
+    if (user) { //if signed in
+        //alert(`you are logged in as ${user.email}`)
+    } else { //if signed out
+        //alert('you are logged out')
     }
+})
+
+// Login modal visibility
+function close_modal(modal_id) {
+    //close modal by removing .is_active
+    document.querySelector(`#${modal_id}`).classList.remove("is-active")
+}
+
+btnlogin.addEventListener('click', () => {
+    loginmodal.classList.add('is-active');
+});
+
+loginmodalbg.addEventListener('click', () => {
+    loginmodal.classList.remove('is-active');
+});
+
+//Login Functionality
+loginform.addEventListener('submit', (e) => {
+    //prevent auto refresh on the page
+    e.preventDefault();
+
+    // grab email and password
+    const logemail = document.querySelector('#log_email').value;
+    const logpass = document.querySelector('#log_password').value;
+
+    auth.signInWithEmailAndPassword(logemail, logpass).then(credentials => {
+        close_modal("login-modal")
+        alert(`you are logged in as ${credentials.user.email}`)
+
+        // reset the form
+        signup_form.reset();
+    }).catch(err => {
+        // display error message on modal
+        login_error.innerHTML = `<p>${err.message}</p>`;
+    })
+
 })
